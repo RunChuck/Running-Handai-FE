379ca6a4c26daf3b334abfcd65472106120c6f6b
InKyungWoo <chelsea1201@naver.com>
2025-06-18 01:17:10 +0900

    ✨ [SCRUM-145] Feat: main 페이지 라우터 설정 및 연결 (#7)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import Login from '@/pages/Auth/Login';
+
+import Login from '@/pages/auth/Login';
+import Main from '@/pages/main/Main';
+import Test from '@/pages/temp/Test';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/test" element={<Test />} />
     </Routes>
   );
 };
