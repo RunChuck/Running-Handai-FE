fd6de35ee963a41e766b4ba5ae46ca68ee19bcb5
InKyungWoo <chelsea1201@naver.com>
2025-07-06 02:12:35 +0900

    🔧 [SCRUM-189] Chore: course api, 관련 type 추가 (#17)

--- src/types/course.ts
+++ src/types/course.ts
@@ -6,6 +6,56 @@ export interface CourseTabItem {
 }
 
 export interface CourseData {
+  thumbnailUrl: string;
+  distanceFromUser: number;
+  distance: number;
+  maxElevation: number;
+  id: number;
+  duration: number;
+}
+
+export interface CourseRequest {
+  filter: string;
+  lat: number;
+  lon: number;
+  area?: string;
+  theme?: string;
+}
+
+export interface CourseResponse {
+  statusCode: number;
+  responseCode: string;
+  message: string;
+  totalCount: number;
+  data: CourseData[];
+}
+
+interface TrackPoint {
+  lat: number;
+  lon: number;
+  ele: number;
+}
+
+interface CourseDetailData {
+  courseId: number;
+  distance: number;
+  duration: number;
+  minelevation: number;
+  maxElevation: number;
+  level: string;
+  roadConditions: string[];
+  trackPoints: TrackPoint[];
+}
+
+export interface CourseDetailResponse {
+  statusCode: number;
+  responseCode: string;
+  message: string;
+  totalCount: number;
+  data: CourseDetailData;
+}
+
+export interface CourseMockData {
   id: number;
   title: string;
   thumbnail: string;
