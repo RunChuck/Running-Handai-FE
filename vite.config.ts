e342530f35b4733e7a5a56bd693b07c49de04b40
InKyungWoo <chelsea1201@naver.com>
2025-07-17 23:58:23 +0900

    🔧 [SCRUM-202] Chore: vite.config 재설정

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_ROOT,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
