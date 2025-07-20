990e159e82336e881fea425ca40af8045f59c699
InKyungWoo <chelsea1201@naver.com>
2025-07-21 00:55:03 +0900

    ✨ [SCRUM-203] Feat: auth 페이지 구현 및 상태 처리 (#24)

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Login from '@/pages/auth/Login';
+import Auth from '@/pages/auth/Auth';
 import Main from '@/pages/main/Main';
 import CourseDetail from '@/pages/course/CoursDetail';
 import Test from '@/pages/temp/Test';
@@ -9,6 +10,7 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/auth" element={<Auth />} />
       <Route path="/main" element={<Main />} />
       <Route path="/course-detail/:id" element={<CourseDetail />} />
       <Route path="/test" element={<Test />} />
