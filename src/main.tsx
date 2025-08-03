46783bf9ab9f17b8aff8e476d3b415aaf9eaed68
InKyungWoo <chelsea1201@naver.com>
2025-08-03 18:30:39 +0900

    ✨ [SCRUM-224] Feat: kakao sdk 설정 및 util 구현 (#34)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx'
+import { initKakao } from './utils/kakao';
+import App from './App.tsx';
+
+// 카카오 SDK 초기화
+initKakao();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
