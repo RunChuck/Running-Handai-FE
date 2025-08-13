a05907943c0c12ccb9abc325f566a74cb181b77a
Inkyung <chelsea1201@naver.com>
2025-08-14 04:12:47 +0900

    ✨ [SCRUM-245] Feat: 러닝 용어 사전 ToolTip 컴포넌트 구현 (#54)

--- src/pages/mypage/terms/TermsPage.tsx
+++ src/pages/mypage/terms/TermsPage.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import styled from '@emotion/styled';
 
 import Header from '../components/Header';
+import ToolTip from './ToopTip';
 
 const TermsPage = () => {
   const [t] = useTranslation();
@@ -11,10 +12,15 @@ const TermsPage = () => {
   return (
     <Container>
       <Header title={t('mypage.terms.title')} onBack={() => navigate(-1)} />
+      <ToolTip />
     </Container>
   );
 };
 
 export default TermsPage;
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-bottom: var(--spacing-32);
+`;
