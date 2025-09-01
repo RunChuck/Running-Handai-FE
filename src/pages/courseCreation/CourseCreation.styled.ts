0f1c6595b96d1cc02ef458b6e6b437b8a3d61db9
Inkyung <chelsea1201@naver.com>
2025-09-01 22:34:20 +0900

    🚨 Hotfix: 모바일 뷰포트 수정

--- src/pages/courseCreation/CourseCreation.styled.ts
+++ src/pages/courseCreation/CourseCreation.styled.ts
@@ -5,4 +5,10 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100vh;
+  height: 100dvh; /* 모바일에서 동적 뷰포트 높이 사용 */
+
+  @supports not (height: 100dvh) {
+    /* dvh를 지원하지 않는 브라우저를 위한 fallback */
+    height: 100vh;
+  }
 `;
